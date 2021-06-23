7449156686e0a269f1cc3c566f5bf1d294012fe0
Stijn <stijn@ardusit.nl>
2021-06-23 12:18:34 +0200

    Am on fire

--- resources/content/nav.php
+++ resources/content/nav.php
@@ -1,17 +1,14 @@
-
-<header class="border border-secondary m-3">
-    <nav class="navbar navbar-expand-lg navbar-dark dark-bg">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Planningstool</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link" href="index.php">Home</a>
-                    <a class="nav-link" href="agenda.php">Agenda</a>
-                </div>
+<nav class="navbar navbar-expand-lg navbar-dark dark-bg border border-secondary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Planningstool</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link" href="index.php">Home</a>
+                <a class="nav-link" href="agenda.php">Agenda</a>
             </div>
         </div>
-    </nav>
-</header>
+    </div>
+</nav>
--- resources/functions/functions.php
+++ resources/functions/functions.php
@@ -9,4 +9,30 @@ function getAllGames(){
     return $result;
 }
 
-?>
+function getGame($id){
+    $pdo = dbCon();
+    $sql = 'SELECT * FROM games WHERE id=:id';
+    $result = $pdo->prepare($sql);
+    $result->bindParam(':id', $id);
+    $result->execute();
+    $result = $result->fetch();
+    return $result;
+}
+
+function appointment($gameName,$datum,$instructor,$players){
+    echo $gameName;
+    echo $datum;
+    echo $instructor;
+    echo $players;
+    $pdo = dbCon();
+    $sql = "INSERT INTO agenda (`gameName`, `date`, `instructor`, `players`) VALUES (:gameName, :date, :instructor, :players)";
+    echo $sql;
+    $result = $pdo->prepare($sql);
+    $result->execute(array(
+        ':gameName' => $gameName,
+        ':date' => date('Y-m-d H:i:s', strtotime($datum)),
+        ':instructor' => $instructor,
+        ':players' => $players
+    )
+    );
+}
